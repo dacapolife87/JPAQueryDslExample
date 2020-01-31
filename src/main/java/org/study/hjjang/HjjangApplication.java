b4b1128f24e28db1d52da622ac4c85084ba10d68
hJJang <dacapolife87@gmail.com>
2020-01-31 10:39:29 +0900

    first commit

---
+++ src/main/java/org/study/hjjang/HjjangApplication.java
@@ -0,0 +1,13 @@
+package org.study.hjjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjjangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjjangApplication.class, args);
+    }
+
+}
