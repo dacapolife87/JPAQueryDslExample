d361cbd55e3dc8973f47dd1f6a42bcf309d0364b
hJJANG <dacapolife87@gmail.com>
2020-02-05 12:38:59 +0900

    QueryDsl Example 20200205

--- src/test/java/org/study/hjjang/HjjangApplicationTests.java
+++ src/test/java/org/study/hjjang/HjjangApplicationTests.java
@@ -1,13 +1,42 @@
 package org.study.hjjang;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import org.study.hjjang.entity.Hello;
+import org.study.hjjang.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
+@Commit
 class HjjangApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        QHello qHello = new QHello("h");
+
+        Hello result = query.selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
